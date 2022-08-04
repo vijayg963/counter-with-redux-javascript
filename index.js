d9ae836419acca199a35376258942c8503c6eb2a
Vijay Gupta <vijayg963@gmail.com>
2022-08-05 00:08:00 +0530

    counter-with-reduxjs

--- index.js
+++ index.js
@@ -1,58 +1,89 @@
-const store = Redux.createStore(
-  reducer /* preloadedState, */,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+let store = Redux.createStore(reducer);
 
-// const store = Redux.createStore(reducer);
-let counter = store.getState();
-let step = store.getState();
-
-let h1 = document.querySelector('h1');
+let counter = document.querySelector('.count');
+let btn5 = document.querySelector('.btn-5');
+let btn10 = document.querySelector('.btn-10');
+let btn15 = document.querySelector('.btn-15');
+let btnMax15 = document.querySelector('.btn-max-15');
+let btnMax100 = document.querySelector('.btn-max-100');
+let btnMax200 = document.querySelector('.btn-max-200');
 let increment = document.querySelector('.increment');
 let decrement = document.querySelector('.decrement');
 let reset = document.querySelector('.reset');
-let first = document.querySelector('.first');
-let second = document.querySelector('.second');
-let third = document.querySelector('.third');
 
-h1.innerText = counter;
+let fixSteps = 1;
+let maxValue = Infinity;
 
-first.addEventListener('click', () => {
-  store.dispatch({ type: 'increment', step: 5 });
+increment.addEventListener(`click`, () => {
+  store.dispatch({ type: 'increment', step: fixSteps || 1 });
 });
 
-second.addEventListener('click', () => {
-  store.dispatch({ type: 'increment', step: 15 });
+decrement.addEventListener(`click`, () => {
+  store.dispatch({ type: 'decrement', step: fixSteps || 1 });
 });
 
-third.addEventListener('click', () => {
-  store.dispatch({ type: 'increment', step: 25 });
-});
-
-increment.addEventListener('click', () => {
-  store.dispatch({ type: 'increment' });
-});
-decrement.addEventListener('click', () => {
-  store.dispatch({ type: 'decrement' });
-});
-reset.addEventListener('click', () => {
+reset.addEventListener(`click`, () => {
   store.dispatch({ type: 'reset' });
+  btn5.classList.remove(`active`);
+  btn10.classList.remove(`active`);
+  btn15.classList.remove(`active`);
+  btnMax15.classList.remove(`active`);
+  btnMax100.classList.remove(`active`);
+  btnMax200.classList.remove(`active`);
+  fixSteps = 1;
+  maxValue = Infinity;
 });
 
 store.subscribe(() => {
-  counter = store.getState();
-  h1.innerText = counter;
+  counter.innerText = store.getState();
 });
 
 function reducer(state = 0, action) {
   switch (action.type) {
     case 'increment':
-      return state + (action.step || 1);
+      return state + action.step <= maxValue ? state + action.step : state;
     case 'decrement':
-      return state - (action.step || 1);
+      return state - action.step >= 0 ? state - action.step : state;
     case 'reset':
       return 0;
     default:
       return state;
   }
 }
+
+btn5.addEventListener(`click`, () => {
+  btn5.classList.add(`active`);
+  btn10.classList.remove(`active`);
+  btn15.classList.remove(`active`);
+  fixSteps = 5;
+});
+btn10.addEventListener(`click`, () => {
+  btn5.classList.remove(`active`);
+  btn10.classList.add(`active`);
+  btn15.classList.remove(`active`);
+  fixSteps = 10;
+});
+btn15.addEventListener(`click`, () => {
+  btn5.classList.remove(`active`);
+  btn10.classList.remove(`active`);
+  btn15.classList.add(`active`);
+  fixSteps = 15;
+});
+btnMax15.addEventListener(`click`, () => {
+  btnMax15.classList.add(`active`);
+  btnMax100.classList.remove(`active`);
+  btnMax200.classList.remove(`active`);
+  maxValue = 15;
+});
+btnMax100.addEventListener(`click`, () => {
+  btnMax15.classList.remove(`active`);
+  btnMax100.classList.add(`active`);
+  btnMax200.classList.remove(`active`);
+  maxValue = 100;
+});
+btnMax200.addEventListener(`click`, () => {
+  btnMax15.classList.remove(`active`);
+  btnMax100.classList.remove(`active`);
+  btnMax200.classList.add(`active`);
+  maxValue = 200;
+});
